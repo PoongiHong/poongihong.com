c01f0538b20bc96acec9e3bee18f89f9f2b30399
webozza <webozza@gmail.com>
2022-09-09 23:42:27 +0800

    popup for annotation 1 included

--- partials/floorplan-sketchfab.php
+++ partials/floorplan-sketchfab.php
@@ -2,6 +2,56 @@
 
 <?php get_header(); ?>
 
+<style>
+    .popup {
+        width: 100vw;
+        height: calc(100vh - 80px);
+        background: rgba(0,0,0,0.8);
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        display: none;
+        flex-direction: column;
+    }
+    h1 {
+        color: #fff;
+    }
+    #api-frame {
+        width: 100vw;
+        height: calc(100vh - 80px);
+    }
+    #close-popup {
+        background: #fff;
+        border-radius: 50%;
+        color: #000;
+        cursor: pointer;
+        width: 50px;
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2em;
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        transition: all .3s ease;
+    }
+    #close-popup:hover {
+        background: #000;
+        color: #fff;
+    }
+
+</style>
+
+<div class="popup">
+    <h1> Testing deployment from git </h1>
+    <div class="inner-container">
+        <div id="close-popup">
+            <span>X</span>
+        </div>
+    </div>
+</div>
+
 <iframe src="" id="api-frame" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 
 <script>
