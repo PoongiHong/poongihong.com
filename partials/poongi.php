d513957a8885471922b7042cf7d5623fe6d0c972
webozza <webozza@gmail.com>
2022-09-09 12:57:37 +0800

    testing fbx to gltf with three

--- partials/poongi.php
+++ partials/poongi.php
@@ -11,7 +11,7 @@
 // GLTF
 var loader = new THREE.GLTFLoader();
 loader.load(
-  "<?= get_stylesheet_directory_uri() . '/scene.gltf' ?>",
+  "<?= get_stylesheet_directory_uri() . '/2nd Floor.gltf' ?>",
   function (gltf) {
     scene.add(gltf.scene);
   }
