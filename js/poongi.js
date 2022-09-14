d0c509c2baf4cdcdfe1b37a561918ae5132fe51f
webozza <webozza@gmail.com>
2022-09-15 07:27:47 +0800

    prev next current lang buttons added

--- js/poongi.js
+++ js/poongi.js
@@ -35,9 +35,8 @@ client.init(uid, {
           }
         });
       });
-      // Hide Viewer Controls
     });
-    // annotation 1 gets clicked
+    // annotation gets clicked
     api.addEventListener("annotationSelect", function (index) {
       window.console.log("Selected annotation", index);
       if (index !== -1) {
@@ -45,17 +44,42 @@ client.init(uid, {
         setTimeout(function () {
           $(".popup").fadeIn("slow").css("display", "flex");
         }, 1000);
-        // Get Content
-        api.getAnnotation(index, function (err, information) {
-          if (!err) {
-            window.console.log(information);
-            var annoTitle = information.name;
-            var annoContent = information.content.rendered;
-            $(".popup .anno-title").text(annoTitle);
-            $(".popup .anno-content").html(annoContent);
-          }
-        });
       }
+      // Get Content
+      api.getAnnotation(index, function (err, information) {
+        if (!err) {
+          window.console.log(information);
+          var annoTitle = information.name;
+          var annoContent = information.content.rendered;
+          $(".popup .anno-title").text(annoTitle);
+          $(".popup .anno-content").html(annoContent);
+          $("#current-anno").text(index + 1 + ": " + annoTitle);
+        }
+      });
+      // Next Room Function
+      $("#next-anno").on("click", function () {
+        api.gotoAnnotation(
+          index + 1,
+          { preventCameraAnimation: false, preventCameraMove: false },
+          function (err, index) {
+            if (!err) {
+              window.console.log("Going to annotation", index + 1);
+            }
+          }
+        );
+      });
+      // Previous Room Function
+      $("#prev-anno").on("click", function () {
+        api.gotoAnnotation(
+          index - 1,
+          { preventCameraAnimation: false, preventCameraMove: false },
+          function (err, index) {
+            if (!err) {
+              window.console.log("Going to annotation", index - 1);
+            }
+          }
+        );
+      });
     });
   },
   error: function onError() {
--- partials/poongi.php
+++ partials/poongi.php
@@ -15,5 +15,9 @@
 <iframe src="" id="api-frame" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 <div class="model-controls">
     <button id="reset-camera" class="poongi-btn">Reset Camera</button>
+    <button id="prev-anno" class="poongi-btn">Previous Room</button>
+    <button id="current-anno" class="poongi-btn">Select Room</button>
+    <button id="next-anno" class="poongi-btn">Next Room</button>
+    <button id="select-lang" class="poongi-btn">Language</button>
 </div>
 <script src="<?= get_stylesheet_directory_uri() . '/js/poongi.js'?>"></script>
